197e9eab1ba55a724b712dcfbe44d6389f09be7d
Alberto Roura <mail@albertoroura.com>
2025-01-12 17:25:43 +0100

    Added quick totals on dashboard

--- src/views/dashboard/Invoices.vue
+++ src/views/dashboard/Invoices.vue
@@ -2,7 +2,22 @@
     <div>
         <div class="row">
             <div class="col-12 mb-4 pr-0 d-flex justify-content-between">
-                <h4 class="mb-0">{{ $t('title') }}</h4>
+              <h4 class="mb-0">
+                {{ $t('title') }}
+                <span class="badge font-weight-light">
+                  <span class="text-info" v-b-tooltip.hover title="Total Invoiced">
+                    {{ formattedTotalMoney }} ({{ totalInvoices }})
+                  </span>
+                  <span class="text-secondary mx-2">|</span>
+                  <span class="text-success" v-b-tooltip.hover title="Total Paid">
+                    {{ formattedTotalMoneyPaid }} ({{ totalPaidInvoices }})
+                  </span>
+                  <span class="text-secondary mx-2">|</span>
+                  <span class="text-danger" v-b-tooltip.hover title="Total Pending">
+                    {{ formattedTotalMoneyPending }} ({{ totalPendingInvoices }})
+                  </span>
+                </span>
+              </h4>
                 <div>
                     <button class="btn btn-sm btn-outline-dark"
                             :class="{ 'mr-3': !isStorageLocal }"
@@ -27,7 +42,7 @@
 </template>
 
 <script>
-import { BDropdown, BDropdownItem } from 'bootstrap-vue';
+import { BDropdown, BDropdownItem, VBTooltip } from 'bootstrap-vue';
 import { mapGetters } from 'vuex';
 import InvoicesList from '@/components/invoices/InvoicesList';
 import config from '@/config/app.config';
@@ -40,12 +55,47 @@ export default {
     BDropdown,
     BDropdownItem,
   },
+  directives: {
+    'b-tooltip': VBTooltip,
+  },
   computed: {
     ...mapGetters({
       team: 'teams/team',
+      invoices: 'invoices/all',
     }),
     isStorageLocal() {
       return config.storageType === 'local';
+    },
+    totalPendingInvoices() {
+      return this.invoices.filter(invoice => invoice.status === 'sent' || invoice.status === 'booked').length;
+    },
+    totalPaidInvoices() {
+      return this.invoices.filter(invoice => invoice.status === 'paid').length;
+    },
+    totalInvoices() {
+      return this.invoices.length;
+    },
+    totalMoneyPending() {
+      return this.invoices
+        .filter(invoice => invoice.status === 'sent' || invoice.status === 'booked')
+        .reduce((sum, invoice) => sum + invoice.total, 0).toFixed(0);
+    },
+    totalMoneyPaid() {
+      return this.invoices
+        .filter(invoice => invoice.status === 'paid')
+        .reduce((sum, invoice) => sum + invoice.total, 0).toFixed(0);
+    },
+    totalMoney() {
+      return this.invoices.reduce((sum, invoice) => sum + invoice.total, 0).toFixed(0);
+    },
+    formattedTotalMoney() {
+      return this.formatCurrency(this.totalMoney);
+    },
+    formattedTotalMoneyPending() {
+      return this.formatCurrency(this.totalMoneyPending);
+    },
+    formattedTotalMoneyPaid() {
+      return this.formatCurrency(this.totalMoneyPaid);
     },
   },
   methods: {
@@ -58,6 +108,14 @@ export default {
     },
     openImportModal() {
       this.$store.commit('data/isImportModalOpen', true);
+    },
+    formatCurrency(value) {
+      return new Intl.NumberFormat('en-AU', {
+        style: 'currency',
+        currency: 'AUD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      }).format(value);
     },
   },
 };
